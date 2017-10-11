9fc77094cf84a6885f44c3fd16052b0e6f9005f7
krrishnaaaa <krishna@pcsalt.com>
2017-10-11 07:17:12 +0530

    instead of password field, using email

--- app/src/main/java/com/pcsalt/example/customalertdialog/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/customalertdialog/MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
     public void buttonClicked(View view) {
         LayoutInflater inflater = getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.layout_custom_dialog, null);
-        final EditText etUsername = (EditText) alertLayout.findViewById(R.id.et_username);
-        final EditText etPassword = (EditText) alertLayout.findViewById(R.id.et_password);
-        final CheckBox cbShowPassword = (CheckBox) alertLayout.findViewById(R.id.cb_show_password);
+        final EditText etUsername = alertLayout.findViewById(R.id.et_username);
+        final EditText etPassword = alertLayout.findViewById(R.id.et_email);
+        final CheckBox cbShowPassword = alertLayout.findViewById(R.id.cb_show_pass);
 
         cbShowPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
@@ -33,16 +33,16 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // to encode password in dots
-                    etPassword.setTransformationMethod(null);
+                    etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
                 } else {
                     // to display the password in normal text
-                    etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    etPassword.setTransformationMethod(null);
                 }
             }
         });
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Login");
+        alert.setTitle("Info");
         // this is set the view from XML inside AlertDialog
         alert.setView(alertLayout);
         // disallow cancel of AlertDialog on click of back button and outside touch
@@ -55,13 +55,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        alert.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+        alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String user = etUsername.getText().toString();
                 String pass = etPassword.getText().toString();
-                Toast.makeText(getBaseContext(), "Username: " + user + " Password: " + pass, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "Username: " + user + " Email: " + pass, Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog dialog = alert.create();
