a9e7fb4e69e6ced4d4893d0c5afefccf23de01cd
krrishnaaaa <krishna@pcsalt.com>
2020-05-01 16:45:44 +0530

    migrated to androidx

--- app/src/main/java/com/pcsalt/example/customalertdialog/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/customalertdialog/MainActivity.java
@@ -2,8 +2,8 @@ package com.pcsalt.example.customalertdialog;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
